a1a4c0fef83a2fdfb8c78e0d7e625ae373bcd7e1
Bri <bhilliker@gmail.com>
2021-06-16 14:38:18 -0500

    add while loops

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,43 @@ var smallLetters =   ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 var bigLetters =   ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var characters = [`!`, `#`, `$`, `&`, `*`, `+`, `-`, `.`, `=`, `?`, `^`, `~`]
 var numbers = [Array.from(Array(10).keys())]
+console.log (bigLetters)
+console.log (smallLetters)
+console.log (characters)
+console.log (numbers)
+possibleCharacters = []
+generatedPassword = []
 
+
+var passwordLength = prompt ("How many characters would you like in your password? (between 8 - 128");
+
+console.log (passwordLength)
+
+//must select a number between 8 and 128
+while (passwordLength < 8 || passwordLength > 128 ) {
+  passwordLength = prompt ("That is not a valid number. Please try again.");
+}
+
+var lowerLetters = confirm ("Would you like to use lowercase letters in your password?");
+
+var upperLetters = confirm ("Would you like to use upper case letters in your password?");
+
+var num = confirm ("Would you like to use numbers in your password?");
+
+var specialChar = confirm ("Would you like to use special characters in your password?");
+
+// must select characters to be in the password
+while (!lowerLetters && !upperLetters && !number && !specialChar) {
+  alert ("You must select at least 1 choice for your password. Please try again.");
+  
+  lowerLetters = confirm ("Would you like to use lowercase letters in your password?");
+
+  upperLetters = confirm ("Would you like to use upper case letters in your password?");
+
+  number = confirm ("Would you like to use numbers in your password?");
+
+  specialChar = confirm ("Would you like to use special characters in your password?");
+}
 
 
 
