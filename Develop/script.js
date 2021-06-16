ed38393ea007f5834c69e798f206daea64367b97
Bri <bhilliker@gmail.com>
2021-06-16 14:36:29 -0500

    add arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
 
+var smallLetters =   ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var bigLetters =   ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var characters = [`!`, `#`, `$`, `&`, `*`, `+`, `-`, `.`, `=`, `?`, `^`, `~`]
+var numbers = [Array.from(Array(10).keys())]
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
